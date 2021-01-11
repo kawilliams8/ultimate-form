fb4852781c7175b84ec749ace77aa7cd398be373
Katie Williams <kawilliams8@gmail.com>
2021-01-10 20:06:00 -0700

    Update App to display <Header/>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Routeer, Route, Switch } from 'react-router-dom';
+import Header from "./components/Header";
+import { Step1 } from './Step1';
+import { Step2 } from './Step2';
+import { Step3 } from './Step3';
+import { Result } from './Result';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Step1} />
-        <Route path="/step2" component={Step2} />
-        <Route path="/step3" component={Step3} />
-        <Route path="/result" component={Result} />
-      </Switch>
-    </Router>
+    <>
+      <Header/>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Step1} />
+          <Route path="/step2" component={Step2} />
+          <Route path="/step3" component={Step3} />
+          <Route path="/result" component={Result} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
