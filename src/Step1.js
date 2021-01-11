24fabe25bf1f10b0e0066e397bc702151170f2b6
Katie Williams <kawilliams8@gmail.com>
2021-01-10 20:17:33 -0700

    Update imports and define Step1 layout

--- src/Step1.js
+++ src/Step1.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
 
 export const Step1 = () => {
-  return null;
+  const { register, handleSubmit, errors } = useForm();
+  const history = useHistory();
+
+  const onSubmit = data => {
+    history.push("/step2")
+  }
+
+  return (
+    <div>
+      <h2>Step 1</h2>
+      <form>
+        <input
+          ref={register}
+          name="firstName"
+          type="text"
+          placeholder="First Name"
+        />
+        <input
+          ref={register}
+          name="lastName"
+          type="text"
+          placeholder="Last Name"
+        />
+        <button type="submit">Next</button>
+      </form>
+    </div>
+  );
 };
