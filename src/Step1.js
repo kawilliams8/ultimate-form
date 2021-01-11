70c2e9fe707ff1f6461af3131213adbf06e4511c
Katie Williams <kawilliams8@gmail.com>
2021-01-10 21:11:01 -0700

    Incorporate yupResolver validation on first and last name inputs, update regex pattern

--- src/Step1.js
+++ src/Step1.js
@@ -4,16 +4,20 @@ import { MainContainer } from './components/MainContainer';
 import { Form } from './components/Form';
 import { Input } from './components/Input';
 import { PrimaryButton } from './components/PrimaryButton';
-import Typography from "@material-ui/core/Typography";
+import Typography from '@material-ui/core/Typography';
+import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
-const schema = yup.object.shape({
-  firstName: yup.string().matches(/^([^0-9])$/, "First name cannot contain numbers").required("First name is required."),
-  lastName: yup.string().matches(/^([^0-9])$/, "Last name cannot contain numbers").required("Last name is required.")
+const schema = yup.object().shape({
+  firstName: yup.string().matches(/^([^0-9]*)$/, "First name cannot contain numbers").required("First name is required."),
+  lastName: yup.string().matches(/^([^0-9]*)$/, "Last name cannot contain numbers").required("Last name is required.")
 })
 
 export const Step1 = () => {
-  const { register, handleSubmit, errors } = useForm();
+  const { register, handleSubmit, errors } = useForm({
+    mode: "onBlur",
+    resolver: yupResolver(schema)
+  });
   const history = useHistory();
 
   const onSubmit = data => {
@@ -26,8 +30,8 @@ export const Step1 = () => {
         👩‍💻 Step 1 👩‍💻
       </Typography>
       <Form>
-        <Input ref={register} name="firstName" type="text" label="First Name" />
-        <Input ref={register} name="lastName" type="text" label="Last Name" />
+        <Input ref={register} name="firstName" type="text" label="First Name" error={!!errors.firstName} helperText={errors?.firstName?.message}/>
+        <Input ref={register} name="lastName" type="text" label="Last Name" error={!!errors.lastName} helperText={errors?.lastName?.message}/>
         <PrimaryButton type="submit">Next</PrimaryButton>
       </Form>
     </MainContainer>
