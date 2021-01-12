176f67794d2eb5fd4f5c3c8e828f3a0501f5fd4f
Katie Williams <kawilliams8@gmail.com>
2021-01-11 18:41:59 -0700

    Add conditional phone input, on checkbox status

--- src/Step2.js
+++ src/Step2.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { MainContainer } from './components/MainContainer';
-import { Form } from './components/Form';
-import { Input } from './components/Input';
-import { useData } from './DataContext';
-import { useForm } from 'react-hook-form';
-import { useHistory } from 'react-router-dom';
-import Typography from '@material-ui/core/Typography';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-
+import { MainContainer } from "./components/MainContainer";
+import { Form } from "./components/Form";
+import { Input } from "./components/Input";
+import { useData } from "./DataContext";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 
 export const Step2 = () => {
   const { data, setValues } = useData();
@@ -17,27 +16,45 @@ export const Step2 = () => {
     defaultValues: {
       email: data.email,
       hasPhone: data.hasPhone,
-      phoneNumber: data.phoneNumber
+      phoneNumber: data.phoneNumber,
     },
-    mode: 'onBlur'
+    mode: "onBlur",
   });
 
-  const hasPhone = watch('hasPhone');
+  const hasPhone = watch("hasPhone");
 
   return (
     <MainContainer>
       <Typography>👩‍💻 Step 2 👩‍💻</Typography>
       <Form>
-        <Input ref={register} type="email" name="email" label="Email" required/>
-        <FormControlLabel control={<Checkbox
-          defaultChecked={data.hasPhone}
-          defaultValue={data.hasPhone}
-          color="primary"
-          inputRef={register}
-          name="hasPhone"
-          />}
-        label="Do you have a phone?"
+        <Input
+          ref={register}
+          type="email"
+          name="email"
+          label="Email"
+          required
         />
+        <FormControlLabel
+          control={
+            <Checkbox
+              defaultChecked={data.hasPhone}
+              defaultValue={data.hasPhone}
+              color="primary"
+              inputRef={register}
+              name="hasPhone"
+            />
+          }
+          label="Do you have a phone?"
+        />
+        {hasPhone && (
+          <Input
+            ref={register}
+            type="tel"
+            id="phoneNumber"
+            name="tel"
+            label="Phone Number"
+          />
+        )}
       </Form>
     </MainContainer>
   );
