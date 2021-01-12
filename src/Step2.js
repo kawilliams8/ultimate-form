72a498c94c2db5c341bb0da22a654772e27a6457
Katie Williams <kawilliams8@gmail.com>
2021-01-11 18:22:03 -0700

    Update layout for Step 2, email input

--- src/Step2.js
+++ src/Step2.js
@@ -1,5 +1,15 @@
 import React from "react";
+import MainContainer from './components/MainContainer';
+import Typography from '@material-ui/core/Typography';
+
 
 export const Step2 = () => {
-  return null;
+  return (
+    <MainContainer>
+      <Typography>👩‍💻 Step 2 👩‍💻</Typography>
+      <Form>
+        <Input type="email" name="email" label="Email" required/>
+      </Form>
+    </MainContainer>
+  );
 };
