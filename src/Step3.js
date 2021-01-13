6cd99da4a8f25dc9231c63dccc2a80f52d113c94
Katie Williams <kawilliams8@gmail.com>
2021-01-12 18:32:22 -0700

    Begin adding layout components to Step 3

--- src/Step3.js
+++ src/Step3.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { MainContainer } from './components/MainContainer';
+import { Form } from './components/Form';
+import { FormInput } from './components/FormInput';
+import Typography from '@material-ui/core/Typography';
 
 export const Step3 = () => {
-  return null;
+  return (
+    <MainContainer>
+      <Typography>👩‍💻 Step 3 👩‍💻</Typography>
+      <Form>
+        <FileInput name="files" control={control}/>
+      </Form>
+    </MainContainer>
+  );
 };
