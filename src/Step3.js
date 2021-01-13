b1cb32018d64a1037ac3a303eaa4e3f30c7c123b
Katie Williams <kawilliams8@gmail.com>
2021-01-12 18:38:17 -0700

    Bring in data context, set up event handler and router progression

--- src/Step3.js
+++ src/Step3.js
@@ -3,8 +3,23 @@ import { MainContainer } from './components/MainContainer';
 import { Form } from './components/Form';
 import { FormInput } from './components/FormInput';
 import Typography from '@material-ui/core/Typography';
+import { useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 export const Step3 = () => {
+  const history = useHistory()
+  const { data, setValues } = useData()
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      files: data.files
+    }
+  })
+
+  const onSubmit = data => {
+    history.push('/result');
+    setValues(data);
+  }
+
   return (
     <MainContainer>
       <Typography>👩‍💻 Step 3 👩‍💻</Typography>
