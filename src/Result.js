b30d08daca0cfc9c8c1de7166ca0e6ddd3eaffe2
Katie Williams <kawilliams8@gmail.com>
2021-01-16 11:38:08 -0700

    Update Results page layout components

--- src/Result.js
+++ src/Result.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { MainContainer } from './components/MainContainer';
+import Paper from '@material-ui/core/Paper';
+import { Typography, Table, TableCell, TableContainer, TableHead, TableRow  } from '@material-ui/core';
 
 export const Result = () => {
-  return null;
+  return (
+    <MainContainer>
+      <Typography component="h2" variant="h5">
+        👩‍💻 Form Values 👩‍💻
+      </Typography>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Field</TableCell>
+              <TableCell>Value</TableCell>
+            </TableRow>
+          </TableHead>
+        </Table>
+      </TableContainer>
+    </MainContainer>
+  );
 };
