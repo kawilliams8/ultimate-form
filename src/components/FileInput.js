cd027513b0ca5e7ceb5032b3ce88c89196bb82a8
Katie Williams <kawilliams8@gmail.com>
2021-01-16 11:22:31 -0700

    Add custom styles to FileInput components

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,11 +1,30 @@
 import React from "react";
 import { Controller } from "react-hook-form";
 import Dropzone from "react-dropzone";
-import { List, ListItem, ListItemIcon, ListItemText, makeStyles, Paper  } from "@material-ui/core";
+import { List, ListItem, ListItemIcon, ListItemText, Paper  } from "@material-ui/core";
 import InsertDriveFile from "@material-ui/icons/InsertDriveFile";
 import CloudUpload from "@material-ui/icons/CloudUpload";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: '#eee',
+    textAlign: 'center',
+    cursor: 'pointer',
+    color: '#333',
+    padding: '10px',
+    marginTop: '20px'
+  },
+  icon: {
+    marginTop: '16px',
+    color: '#888',
+    fontSize: '42px'
+  }
+})
 
 export const FileInput = ({ control, name }) => {
+  const styles = useStyles();
+
   return (
     <Controller
       control={control}
@@ -15,17 +34,17 @@ export const FileInput = ({ control, name }) => {
         <>
           <Dropzone onDrop={onChange}>
             {({ getRootProps, getInputProps }) => (
-              <Paper variant="outlined" {...getRootProps()}>
-                <CloudUpload />
+              <Paper className={styles.root} variant="outlined" {...getRootProps()}>
+                <CloudUpload className={styles.icon}/>
                 <input {...getInputProps()} name={name} onBlur={onBlur} />
                 <p>Drop files or click to select.</p>
               </Paper>
             )}
           </Dropzone>
           <List>
-            {value.map((file, i) => {
+            {value.map((file, index) => {
               return (
-                <ListItem>
+                <ListItem key={index}>
                   <ListItemIcon>
                     <InsertDriveFile />
                   </ListItemIcon>
