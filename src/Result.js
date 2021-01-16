5ef83650d09f4cbb9debe6af1595d580fef650d2
Katie Williams <kawilliams8@gmail.com>
2021-01-16 11:58:42 -0700

    Fix up data hook syntax, add more layout components

--- src/Result.js
+++ src/Result.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { useData } from './DataContext';
 import { MainContainer } from './components/MainContainer';
-import Paper from '@material-ui/core/Paper';
+import { List, ListItem, ListItemIcon, ListItemText, Paper } from '@material-ui/core';
 import { Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow  } from '@material-ui/core';
+import { PrimaryButton } from "./components/PrimaryButton";
+import { Link } from 'react-router-dom';
 
 export const Result = () => {
-  const data = useDate();
+  const { data } = useData();
 
   const entries = Object.entries(data).filter(entry => entry[0] !== 'files');
-  const files = {data};
+  console.log(entries)
+  const { files } = data;
 
   return (
     <MainContainer>
@@ -23,19 +27,33 @@ export const Result = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {entries.map(entry => (
+            {entries.map((entry) => (
               <TableRow key={entry[0]}>
-                <TableCell>
-                  {entry[0]}
-                </TableCell>
-                <TableCell>
-                  {entry[1].toString()}
-                </TableCell>
+                <TableCell>{entry[0]}</TableCell>
+                <TableCell>{entry[1].toString()}</TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+      {files && (
+        <>
+          <Typography component="h2" variant="h5">
+            🗄️ Files 🗄️
+          </Typography>
+          <List>
+            { files.map((file, index) => (
+              <ListItem key={index}>
+                <ListItemIcon>
+                  <ListItemText primary={file.name} secondary={file.size}/>
+                </ListItemIcon>
+              </ListItem>
+            ))}
+          </List>
+        </>
+      )}
+      <PrimaryButton onClick={onSubmit}>Submit</PrimaryButton>
+      <Link to='/'>StartOver</Link>
     </MainContainer>
   );
 };
